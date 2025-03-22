a5b0d496ea0bf6b523b4072291f25110692cd65b
Bojun Pan <bojunpan@altosradar.com>
2025-03-22 09:55:08 -0700

    fix uart on spl

--- arch/arm/mach-k3/r5/j722s/dev-data.c
+++ arch/arm/mach-k3/r5/j722s/dev-data.c
@@ -50,7 +50,7 @@ static struct ti_dev soc_dev_list[] = {
 	PSC_DEV(75, &soc_lpsc_list[6]),
 	PSC_DEV(36, &soc_lpsc_list[7]),
 	PSC_DEV(102, &soc_lpsc_list[7]),
-	PSC_DEV(146, &soc_lpsc_list[7]),
+	PSC_DEV(156, &soc_lpsc_list[7]),
 	PSC_DEV(13, &soc_lpsc_list[8]),
 	PSC_DEV(166, &soc_lpsc_list[9]),
 	PSC_DEV(135, &soc_lpsc_list[10]),
